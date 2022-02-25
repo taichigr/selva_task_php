f0ca1221752a723cdaaff4440a5b1532005d5931
taichi <taichi.01gr14@gmail.com>
2022-02-25 15:10:20 +0900

    add grayraea back next

--- php/thread_detail.php
+++ php/thread_detail.php
@@ -46,13 +46,21 @@ if(!empty($thread_id)) {
     <div class="container">
         <h2><?php if(!empty($result)) echo $result['title'] ?></h2>
         <p class="thread-detail-time"><?php if(!empty($result)) echo date("m/d/y h:i", strtotime($result['created_at'])) ?></p>
-        <div class="gray-area"></div>
-        <div class="thread-content-area">
+        <div class="gray-area">
+            <div class="gray-area-inner">
+                <a href="">前へ</a>
+                <a href="">次へ</a>
+            </div>
+        </div>        <div class="thread-content-area">
             <p>投稿者：<?php if(!empty($result)) echo $result['name_sei'].$result['name_mei'] ?>　<?php if(!empty($result)) echo date('Y.m.d H:i', strtotime($result['created_at'])) ?></p>
             <p><?php if(!empty($result)) echo $result['content'] ?></p>
         </div>
-        <div class="gray-area"></div>
-
+        <div class="gray-area">
+            <div class="gray-area-inner">
+                <a href="">前へ</a>
+                <a href="">次へ</a>
+            </div>
+        </div>
         <?php
         if(!empty($_SESSION['login_date'])):
             if(($_SESSION['login_date'] + $_SESSION['login_limit']) > time()):
